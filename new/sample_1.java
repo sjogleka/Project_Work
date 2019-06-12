80245ae41de8946a0c6239f8a671427ee85f8c17
System Administrator <root@Sumedhs-MacBook-Pro.local>
2019-06-12 11:23:49 -0600

    Test

---
+++ new/sample_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sample_1 {
+	void methodName(){
+    System.out.println("inside a method, prints fine");
+}
+	public static void main(String[] args) {
+		sample_1 m = new sample_1();
+		m.methodName();
+		
+	}
+}
